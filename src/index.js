96afcab243c7560b2eaaa28f9ecbc0b529dbc00d
Dmitry Dudin <dima.dudin@gmail.com>
2018-08-15 13:22:17 +0700

    Refactored

--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
 import {
+  I,
   eq,
   ne,
   not,
-  reverse,
-  split,
-  last,
-  tail,
-  join,
+  assoc,
   dissoc,
-  keys,
-  prepend,
-  append,
+  prop,
   concat,
+  append,
+  reverse,
+  tail,
+  last,
+  has,
+  keys,
+  filter,
+  find,
+  sort,
   each,
   map,
   reduce,
-  find,
+  join,
+  split,
   apply,
   compose,
   freeze,
+  match,
+  isNull,
+  isDefined,
   isObject
 } from '@nuware/functions'
 
@@ -34,134 +42,177 @@ import Emitter from '@nuware/emitter'
 const Store = (initialState = {}, {
   separator = '/'
 } = {}) => {
+  let state = Object.assign({}, initialState)
   const emitter  = Emitter()
-  const RootLens = Prop('ROOT')
 
-  let state = Set(RootLens)(initialState)({})
+  const EventsLens = Prop('events')
+  const ValueLens = Prop('value')
+  const PathLens = Prop('path')
 
-  const Way = (path = '') => {
-    const xs = eq('')(path) ? [] : split(separator)(path)
-    const origin = join(separator)(reverse(tail(reverse(xs))))
-    const key = last(xs) || ''
+  const pathToArray = (path) => filter(I)(split(separator)(path))
+  const arrayToPath = (xs) => join(separator)(filter(I)(xs))
+  const makeLens = (path = '') => apply(compose)(map(Prop)(pathToArray(path)))
 
-    const lens = apply(compose)(compose(
-      prepend(RootLens),
-      map(Prop)
-    )(xs))
+  const VALUE_EVENT         = 'value'
+  const CHILD_ADDED_EVENT   = 'child-added'
+  const CHILD_CHANGED_EVENT = 'child-changed'
+  const CHILD_REMOVED_EVENT = 'child-removed'
 
-    return freeze({
-      parent: () => Way(origin),
-      origin: () => origin,
-      key: () => key,
-      lens: () => lens,
-      child: (key) => Way(join(separator)([path, key])),
-      toString: () => join(separator)(xs),
-      valueOf: () => get(path)
-    })
-  }
+  const get = (path) => freeze(Get(makeLens(path))(state) || null)
 
-  const buildEventKey = (type, path) => join('://')([type, path])
-
-  const buildEvent = (type, way) => freeze({ type, way })
-
-  const Events = (way, types) => {
-    const emitterStateKeys = keys(emitter.state())
-
-    const buildCreatedEvents = (way, acc = []) => {
-      if (ne('')(way.key())) {
-        const key = buildEventKey('created', way.origin())
-        const found = find(eq(key))(emitterStateKeys)
-        if (found && not(way.valueOf())) {
-          acc = append(buildEvent('created', way))(acc)
-        }
-        return buildCreatedEvents(way.parent(), acc)
-      }
-      return acc
-    }
-
-    const buildRemovedEvents = (way, acc = []) => {
-      const childs = reduce((acc, key) => {
-        return concat(buildRemovedEvents(way.child(key)))(acc)
-      })([])(keys(way.valueOf()))
-      const key = buildEventKey('removed', way.origin())
-      const found = find(eq(key))(emitterStateKeys)
-      if (found) {
-        acc = append(buildEvent('removed', way))(acc)
-        acc = concat(acc)(childs)
-      }
-      return acc
-    }
-
-    const buildChangedEvents = (way, acc = []) => {
-      if (ne('')(way.key())) {
-        const key = buildEventKey('changed', way.toString())
-        const found = find(eq(key))(emitterStateKeys)
-        if (found) {
-          acc = append(buildEvent('changed', way))(acc)
-        }
-        return buildChangedEvents(way.parent(), acc)
-      }
-      return acc
-    }
-
-    const events = {
-      created: buildCreatedEvents,
-      removed: buildRemovedEvents,
-      changed: buildChangedEvents
-    }
-
-    return reduce((acc, type) => {
-      return concat(acc)(apply(events[type])([way]))
-    })([])(types)
-  }
-
-  const emit = (events) => {
-    each(({ type, way }) => {
-      const key = buildEventKey(type, eq(type)('changed') ? way.toString() : way.origin())
-      const payload = freeze({
-        key: way.key(),
-        origin: way.origin(),
-        value: way.valueOf(),
-        path: way.toString()
+  const onChilds = (ref) => {
+    const childs = reduce((acc, key) =>
+      concat(onChilds(ref.child(key)))(acc)
+    )([])(keys(ref.value()))
+    return compose(
+      append({
+        type: VALUE_EVENT,
+        path: ref.path,
+        value: ref.value
+      }),
+      append({
+        type: CHILD_REMOVED_EVENT,
+        path: ref.origin,
+        key: ref.key,
+        value: ref.value
       })
-
-      emitter.emit(key)(payload)
-    })(events)
-
-    return void (0)
-  }
-
-  const get = (path) => {
-    const way = Way(path)
-    return Get(way.lens())(state)
-  }
-
-  const set = (path) => (value) => {
-    if (isObject(value)) {
-      return map((key) => set(join(separator)([path, key]))(Get(Prop(key))(value)))(keys(value))
-    }
-    const way = Way(path)
-    const events = Events(way, ['created', 'changed'])
-    state = Set(way.lens())(value)(state)
-    emit(events)
-    return void (0)
+    )(childs)
   }
 
   const del = (path) => {
-    const way = Way(path)
-    const events = Events(way, ['removed', 'changed'])
-    state = Over(way.parent().lens())(dissoc(way.key()))(state)
-    emit(events)
+    const ref = Ref(path)
+
+    const parent = get(ref.origin)
+    if (not(has(ref.key)(parent))) { return [] }
+    if (isObject(parent) && eq(0)(keys(parent).length)) { return del(ref.origin) }
+
+    const value = dissoc(ref.key)(parent)
+    if (isObject(value) && eq(0)(keys(value).length) && ne('')(ref.origin)) { return del(ref.origin) }
+
+    const events = onChilds(ref)
+    state = Set(makeLens(ref.origin))(value)(state)
+    return events
+  }
+
+  const set = (path, value) => {
+    const ref = Ref(path)
+
+    if (isNull(value)) {
+      return eq('')(ref.path)
+        ? reduce((acc, key) => concat(del(ref.child(key).path))(acc))([])(keys(ref.value()))
+        : del(ref.path)
+    }
+
+    if (isObject(value)) {
+      if (eq(0)(keys(value).length)) { return del(ref.path) }
+
+      const keysForUpdate = keys(value)
+      const keysForDelete = filter((key) => not(find(eq(key))(keysForUpdate)))(keys(ref.value()))
+
+      const deletes = reduce((acc, key) => concat(del(ref.child(key).path))(acc))([])(keysForDelete)
+      const updates = reduce((acc, key) => concat(set(ref.child(key).path, prop(key)(value)))(acc))([])(keysForUpdate)
+
+      return concat(deletes)(updates)
+    }
+
+    const events = ne(ref.value())(value)
+      ? Get(EventsLens)(reduce((acc, k) => {
+        const p = append(k)(Get(PathLens)(acc))
+        const v = Get(ValueLens)(acc)
+        const r = Ref(arrayToPath(p))
+
+        return compose(
+          Set(ValueLens)(r.value()),
+          Set(PathLens)(p),
+          Over(EventsLens)(compose(
+            append({
+              type: VALUE_EVENT,
+              path: r.path,
+              value: r.value
+            }),
+            append({
+              type: has(k)(v) ? CHILD_CHANGED_EVENT : CHILD_ADDED_EVENT,
+              path: r.origin,
+              key: r.key,
+              value: r.value
+            })
+          ))
+        )(acc)
+      })({
+        path: [],
+        events: [],
+        value: state
+      })(pathToArray(ref.path)))
+      : []
+
+    state = Set(makeLens(ref.path))(value)(state)
+
+    return events
+  }
+
+  const emit = (groups) => {
+    const emitterState = emitter.state()
+    each((type) => {
+      each((event) => {
+        const key = join('://')([event.type, event.path])
+        has(key)(emitterState) && emitter.emit(key)(compose(
+          freeze,
+          assoc('value')(event.value()),
+          assoc('path')(event.path),
+          has('key')(event) ? assoc('key')(event.key) : I
+        )({}))
+      })(prop(type)(groups) || [])
+    })([CHILD_REMOVED_EVENT, CHILD_ADDED_EVENT, CHILD_CHANGED_EVENT, VALUE_EVENT])
     return void (0)
   }
 
-  return freeze({
-    set,
-    get,
-    del,
-    on: (type, path) => emitter.on(buildEventKey(type, path)),
-    off: (type, path) => emitter.off(buildEventKey(type, path))
-  })
+  const groupEvents = (events) => {
+    return reduce((acc, ev) => Over(Prop(ev.type))((es = []) =>
+      (eq(VALUE_EVENT)(ev.type) || eq(CHILD_CHANGED_EVENT)(ev.type))
+        ? find((e) => eq(ev.path)(e.path))(es) ? es : append(ev)(es)
+        : append(ev)(es)
+    )(acc)
+    )({})(events)
+  }
+
+  const sortGroups = (groups) => {
+    const Desc = (a, b) => eq(a.path)(b.path) ? 0 : (a.path > b.path) ? -1 : 1
+    const Asc = (a, b) => eq(a.path)(b.path) ? 0 : (a.path < b.path) ? -1 : 1
+
+    return reduce((acc, key) => {
+      const events = prop(key)(groups) || []
+      const sortType = match(key)
+        .on(eq(VALUE_EVENT), () => Desc)
+        .on(eq(CHILD_ADDED_EVENT), () => Asc)
+        .on(eq(CHILD_CHANGED_EVENT), () => Desc)
+        .on(eq(CHILD_REMOVED_EVENT), () => Desc)
+        .otherwise(() => Desc)
+      return assoc(key)(sort(sortType)(events))(acc)
+    })({})(keys(groups))
+  }
+
+  const Ref = (path = '') => {
+    const xs = pathToArray(path)
+    const key = last(xs) || ''
+    const origin = arrayToPath(compose(reverse, tail, reverse)(xs))
+
+    const instance = freeze({
+      key,
+      origin,
+      path: arrayToPath(xs),
+      child: (path) => Ref(arrayToPath(concat(pathToArray(path))(xs))),
+      value: (value) => isDefined(value) ? compose(emit, sortGroups, groupEvents)(set(instance.path, value)) : get(instance.path),
+      remove: () => instance.value(null),
+      on: (type) => (handler) => emitter.on(join('://')([type, path]))(handler),
+      off: (type) => (handler) => emitter.off(join('://')([type, path]))(handler)
+    })
+
+    return instance
+  }
+
+  return {
+    ref: (path) => Ref(path)
+  }
 }
 
 export default Store
