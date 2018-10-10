2704523b603b392f83365ac6d55ab855ed22c50a
Dmitry Dudin <dima.dudin@gmail.com>
2018-10-10 17:44:38 +0700

    Refactored

--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
 import {
   I,
   eq,
-  ne,
   not,
-  assoc,
-  dissoc,
-  prop,
-  concat,
-  append,
-  reverse,
+  has,
+  map,
+  each,
+  keys,
+  join,
   tail,
   last,
-  has,
-  keys,
-  filter,
-  find,
-  sort,
-  each,
-  map,
-  reduce,
-  join,
   split,
   apply,
+  assoc,
+  equal,
+  append,
+  concat,
+  dissoc,
+  filter,
+  reduce,
+  flatten,
+  reverse,
   compose,
-  freeze,
-  match,
-  isNull,
-  isDefined,
-  isObject
+  freeze
 } from '@nuware/functions'
 
 import {
@@ -39,180 +33,153 @@ import {
 
 import Emitter from '@nuware/emitter'
 
-const Store = (initialState = {}, {
-  separator = '/'
-} = {}) => {
-  let state = Object.assign({}, initialState)
-  const emitter  = Emitter()
+const Store = (initialState) => {
+  const PATH_SEPARATOR = '/'
+  const EVENT_SEPARATOR = '://'
 
-  const EventsLens = Prop('events')
-  const ValueLens = Prop('value')
-  const PathLens = Prop('path')
+  const EMPTY_STATE = null
 
-  const pathToArray = (path) => filter(I)(split(separator)(path))
-  const arrayToPath = (xs) => join(separator)(filter(I)(xs))
-  const makeLens = (path = '') => apply(compose)(map(Prop)(pathToArray(path)))
-
-  const VALUE_EVENT         = 'value'
-  const CHILD_ADDED_EVENT   = 'child-added'
+  const VALUE_EVENT = 'value'
+  const CHILD_ADDED_EVENT = 'child-added'
   const CHILD_CHANGED_EVENT = 'child-changed'
   const CHILD_REMOVED_EVENT = 'child-removed'
 
-  const get = (path) => freeze(Get(makeLens(path))(state) || null)
+  const emitter = Emitter()
 
-  const onChilds = (ref) => {
-    const childs = reduce((acc, key) =>
-      concat(onChilds(ref.child(key)))(acc)
-    )([])(keys(ref.value()))
-    return compose(
-      append({
-        type: VALUE_EVENT,
-        path: ref.path,
-        value: ref.value
-      }),
-      append({
-        type: CHILD_REMOVED_EVENT,
-        path: ref.origin,
-        key: ref.key,
-        value: ref.value
-      })
-    )(childs)
+  const Path = (...args) => {
+    const paths = compose(
+      filter(I), split(PATH_SEPARATOR),
+      join(PATH_SEPARATOR), filter(I),
+      flatten
+    )(args)
+    const path = join(PATH_SEPARATOR)(paths)
+    const lens = compose(apply(compose), map(Prop))(paths)
+
+    return freeze({
+      key: () => last(paths) || null,
+      child: (...args) => compose(Path, concat(args))(paths),
+      parent: () => compose(Path, reverse, tail, reverse)(paths),
+      toLens: () => lens,
+      toString: () => path,
+      valueOf: () => paths,
+      inspect: () => `Path(${path})`
+    })
   }
 
-  const del = (path) => {
-    const ref = Ref(path)
-
-    const parent = get(ref.origin)
-    if (not(has(ref.key)(parent))) { return [] }
-    if (isObject(parent) && eq(0)(keys(parent).length)) { return del(ref.origin) }
-
-    const value = dissoc(ref.key)(parent)
-    if (isObject(value) && eq(0)(keys(value).length) && ne('')(ref.origin)) { return del(ref.origin) }
-
-    const events = onChilds(ref)
-    state = Set(makeLens(ref.origin))(value)(state)
-    return events
+  const Notification = (type, path, payload) => {
+    const k = join(EVENT_SEPARATOR)([type, path.toString()])
+    return () => emitter.emit(k)(payload)
   }
 
-  const set = (path, value) => {
-    const ref = Ref(path)
+  let _state = initialState || EMPTY_STATE
+  let _paths = {}
 
-    if (isNull(value)) {
-      return eq('')(ref.path)
-        ? reduce((acc, key) => concat(del(ref.child(key).path))(acc))([])(keys(ref.value()))
-        : del(ref.path)
+  const paths = () => _paths
+  const state = () => _state
+
+  const getIn = (path) => (st) => Get(path.toLens())(st) || EMPTY_STATE
+  const setIn = (path, data) => (st) => Set(path.toLens())(data)(st)
+  const removeIn = (path) => (st) => Over(path.parent().toLens())(dissoc(path.key()))(st)
+
+  const compareState = (oldest, newest) => {
+    const childsNotifications = (type, path, properties, st) => {
+      return reduce((acc, property) => {
+        return append(Notification(type, path, {
+          path: path.toString(),
+          key: property,
+          value: getIn(Path(property))(st)
+        }))(acc)
+      })([])(properties)
     }
 
-    if (isObject(value)) {
-      if (eq(0)(keys(value).length)) { return del(ref.path) }
+    const notifications = reduce((acc, curr) => {
+      const types = Get(Prop(curr))(paths())
 
-      const keysForUpdate = keys(value)
-      const keysForDelete = filter((key) => not(find(eq(key))(keysForUpdate)))(keys(ref.value()))
+      if (eq(0)(keys(types).length)) return acc
 
-      const deletes = reduce((acc, key) => concat(del(ref.child(key).path))(acc))([])(keysForDelete)
-      const updates = reduce((acc, key) => concat(set(ref.child(key).path, prop(key)(value)))(acc))([])(keysForUpdate)
+      const path = Path(curr)
+      const oldestValue = getIn(path)(oldest)
+      const newestValue = getIn(path)(newest)
 
-      return concat(deletes)(updates)
-    }
+      if (has(CHILD_ADDED_EVENT)(types)) {
+        const props = filter((k) => not(has(k)(oldestValue)))(keys(newestValue))
+        acc = concat(childsNotifications(CHILD_ADDED_EVENT, path, props, newestValue))(acc)
+      }
 
-    const events = ne(ref.value())(value)
-      ? Get(EventsLens)(reduce((acc, k) => {
-        const p = append(k)(Get(PathLens)(acc))
-        const v = Get(ValueLens)(acc)
-        const r = Ref(arrayToPath(p))
+      if (has(CHILD_REMOVED_EVENT)(types)) {
+        const props = filter((k) => not(has(k)(newestValue)))(keys(oldestValue))
+        acc = concat(childsNotifications(CHILD_REMOVED_EVENT, path, props, newestValue))(acc)
+      }
 
-        return compose(
-          Set(ValueLens)(r.value()),
-          Set(PathLens)(p),
-          Over(EventsLens)(compose(
-            append({
-              type: VALUE_EVENT,
-              path: r.path,
-              value: r.value
-            }),
-            append({
-              type: has(k)(v) ? CHILD_CHANGED_EVENT : CHILD_ADDED_EVENT,
-              path: r.origin,
-              key: r.key,
-              value: r.value
-            })
-          ))
-        )(acc)
-      })({
-        path: [],
-        events: [],
-        value: state
-      })(pathToArray(ref.path)))
-      : []
+      if (has(CHILD_CHANGED_EVENT)(types)) {
+        const props = compose(
+          filter((k) => not(equal(Get(Prop(k))(oldestValue))(Get(Prop(k))(newestValue)))),
+          filter((k) => has(k)(oldestValue))
+        )(keys(newestValue))
+        acc = concat(childsNotifications(CHILD_CHANGED_EVENT, path, props, newestValue))(acc)
+      }
 
-    state = Set(makeLens(ref.path))(value)(state)
+      if (has(VALUE_EVENT)(types) && not(equal(oldestValue)(newestValue))) {
+        acc = append(Notification(VALUE_EVENT, path, {
+          path: path.toString(),
+          value: newestValue
+        }))(acc)
+      }
 
-    return events
+      return acc
+    })([])(keys(paths()))
+
+    _state = newest
+
+    return notifications
   }
 
-  const emit = (groups) => {
-    const emitterState = emitter.state()
-    each((type) => {
-      each((event) => {
-        const key = join('://')([event.type, event.path])
-        has(key)(emitterState) && emitter.emit(key)(compose(
-          freeze,
-          assoc('value')(event.value()),
-          assoc('path')(event.path),
-          has('key')(event) ? assoc('key')(event.key) : I
-        )({}))
-      })(prop(type)(groups) || [])
-    })([CHILD_REMOVED_EVENT, CHILD_ADDED_EVENT, CHILD_CHANGED_EVENT, VALUE_EVENT])
-    return void (0)
-  }
-
-  const groupEvents = (events) => {
-    return reduce((acc, ev) => Over(Prop(ev.type))((es = []) =>
-      (eq(VALUE_EVENT)(ev.type) || eq(CHILD_CHANGED_EVENT)(ev.type))
-        ? find((e) => eq(ev.path)(e.path))(es) ? es : append(ev)(es)
-        : append(ev)(es)
-    )(acc)
-    )({})(events)
-  }
-
-  const sortGroups = (groups) => {
-    const Desc = (a, b) => eq(a.path)(b.path) ? 0 : (a.path > b.path) ? -1 : 1
-    const Asc = (a, b) => eq(a.path)(b.path) ? 0 : (a.path < b.path) ? -1 : 1
-
-    return reduce((acc, key) => {
-      const events = prop(key)(groups) || []
-      const sortType = match(key)
-        .on(eq(VALUE_EVENT), () => Desc)
-        .on(eq(CHILD_ADDED_EVENT), () => Asc)
-        .on(eq(CHILD_CHANGED_EVENT), () => Desc)
-        .on(eq(CHILD_REMOVED_EVENT), () => Desc)
-        .otherwise(() => Desc)
-      return assoc(key)(sort(sortType)(events))(acc)
-    })({})(keys(groups))
-  }
-
-  const Ref = (path = '') => {
-    const xs = pathToArray(path)
-    const key = last(xs) || ''
-    const origin = arrayToPath(compose(reverse, tail, reverse)(xs))
-
-    const instance = freeze({
-      key,
-      origin,
-      path: arrayToPath(xs),
-      child: (path) => Ref(arrayToPath(concat(pathToArray(path))(xs))),
-      value: (value) => isDefined(value) ? compose(emit, sortGroups, groupEvents)(set(instance.path, value)) : get(instance.path),
-      remove: () => instance.value(null),
-      on: (type) => (handler) => emitter.on(join('://')([type, path]))(handler),
-      off: (type) => (handler) => emitter.off(join('://')([type, path]))(handler)
+  const Ref = (path) => {
+    const ref = freeze({
+      child: (...args) => Ref(path.child(args)),
+      parent: () => Ref(path.parent()),
+      get: () => getIn(path)(state()),
+      set: (data) => {
+        const oldest = getIn(Path())(state())
+        const newest = setIn(path, data)(state())
+        const notifications = compareState(oldest, newest)
+        return each((fn) => apply(fn)())(notifications)
+      },
+      remove: () => {
+        const oldest = getIn(Path())(state())
+        const newest = removeIn(path)(state())
+        const notifications = compareState(oldest, newest)
+        return each((fn) => apply(fn)())(notifications)
+      },
+      off: (type, handler) => {
+        const k = join(EVENT_SEPARATOR)([type, path.toString()])
+        emitter.off(k)(handler)
+        if (not(emitter.has(k, handler))) {
+          _paths = Over(Prop(path.toString()))(dissoc(type))(paths())
+        }
+        return void (0)
+      },
+      on: (type, handler) => {
+        const k = join(EVENT_SEPARATOR)([type, path.toString()])
+        emitter.on(k)(handler)
+        _paths = Over(Prop(path.toString()))(assoc(type)(true))(paths())
+        return () => ref.off(type, handler)
+      },
+      inspect: () => `Ref(${path.toString()})`
     })
 
-    return instance
+    return ref
   }
 
-  return {
-    ref: (path) => Ref(path)
-  }
+  const store = freeze({
+    ref: () => Ref(Path()),
+    CHILD_ADDED_EVENT,
+    CHILD_CHANGED_EVENT,
+    CHILD_REMOVED_EVENT,
+    VALUE_EVENT
+  })
+
+  return store
 }
 
 export default Store
