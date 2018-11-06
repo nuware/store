0896e74ab52265136a85a5b921eecab25067d955
Dmitry Dudin <dima.dudin@gmail.com>
2018-11-06 15:33:47 +0700

    Upgrade dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,57 @@
 import resolve from 'rollup-plugin-node-resolve'
-import minify from 'rollup-plugin-minify-es'
+import commonjs from 'rollup-plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json'
 
-const banner = '/**\n * Store\n *\n * Copyright 2018 Dmitry Dudin <dima@nuware.ru>\n */'
+const input = 'src/index.js'
+const external = ['@nuware/functions', '@nuware/lenses', '@nuware/emitter']
+
+const banner = `/**
+ * Store - ${pkg.description}
+ *
+ * @version ${pkg.version}
+ * @license MIT
+ * @copyright Dmitry Dudin <dima@nuware.ru> 2018
+ */`
 
 export default [{
-  input: 'src/index.js',
+  input,
+  external,
   output: {
-    file: 'dist/store.esm.js',
+    file: pkg.module,
     format: 'esm',
     banner
-  },
-  external: ['@nuware/functions', '@nuware/lenses', '@nuware/emitter']
+  }
 }, {
-  input: 'src/index.js',
+  input,
+  external,
   output: {
-    file: 'dist/store.umd.js',
+    file: pkg.main,
+    format: 'cjs',
+    banner
+  }
+}, {
+  input,
+  output: {
+    file: pkg.browser,
     format: 'umd',
     name: 'nuware.Store',
     banner
   },
   plugins: [
-    resolve()
+    resolve(),
+    commonjs()
   ]
 }, {
-  input: 'src/index.js',
+  input,
   output: {
-    file: 'dist/store.min.js',
+    file: pkg.minimized,
     format: 'umd',
     name: 'nuware.Store'
   },
   plugins: [
     resolve(),
-    minify()
+    commonjs(),
+    terser()
   ]
 }]
